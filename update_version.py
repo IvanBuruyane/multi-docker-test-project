946c0a8afd0a426198054c5110c462d6ef842f52
Ivan Buruyane <ivan@paydock.com>
2021-06-28 21:05:12 +0300

    Added update_version script

--- update_version.py
+++ update_version.py
@@ -1,9 +1,15 @@
-import yaml
+import re
 from pathlib import Path
+
 path = Path(Path.cwd(), ".github", "workflows", "main.yml")
 with open(path, "r") as file:
-    yml = yaml.safe_load(file)
-yml['jobs']['build']['steps'][-1]['with']['version_label'] += 1
+    yml = file.read()
+
+find = re.search(r"version_label:\s*(\d)+\s*\n", yml).group(0)
+n = int(re.search(r"\d+", find).group(0))
+n += 1
+new_string = f"version_label: {n}\n"
+new_yml = re.sub(r"version_label:\s*(\d)+\s*\n", new_string, yml)
 with open(path, "w") as file:
-    yaml.dump(yml, file)
+    file.write(new_yml)
 
