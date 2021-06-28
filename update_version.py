43b453e41b4bb8dc66d2e96cbdc1342dea11b007
Ivan Buruyane <ivan@paydock.com>
2021-06-28 21:22:47 +0300

    Fixed update_version script

--- update_version.py
+++ update_version.py
@@ -1,15 +1,19 @@
+import os
 import re
 from pathlib import Path
 
 path = Path(Path.cwd(), ".github", "workflows", "main.yml")
+print(path)
 with open(path, "r") as file:
     yml = file.read()
 
 find = re.search(r"version_label:\s*(\d)+\s*\n", yml).group(0)
 n = int(re.search(r"\d+", find).group(0))
 n += 1
-new_string = f"version_label: {n}\n"
-new_yml = re.sub(r"version_label:\s*(\d)+\s*\n", new_string, yml)
-with open(path, "w") as file:
-    file.write(new_yml)
+os.environ["VERSION_LABEL"] = f"{n}"
+print("Environment version label: " + os.environ.get("VERSION_LABEL"))
+# new_string = f"version_label: {n}\n"
+# new_yml = re.sub(r"version_label:\s*(\d)+\s*\n", new_string, yml)
+# with open(path, "w") as file:
+#     file.write(new_yml)
 
