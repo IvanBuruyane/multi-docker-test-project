5b612b6d20fe8a591fbaa97dc7add4fcc0d83564
Ivan Buruyane <ivan@paydock.com>
2021-06-30 08:37:03 +0300

    Added increment step to the workflow

--- update_version.py
+++ update_version.py
@@ -1,17 +1,19 @@
 import os
 from pathlib import Path
+import requests
 
-path = Path(Path.cwd(), "version.txt")
-print(path)
+# path = Path(Path.cwd(), "version.txt")
+# print(path)
+#
+# with open(path, "r") as file:
+#     text = file.read()
+#
+# version = int(text)
+# version += 1
+# os.environ["VERSION_LABEL"] = f"{version}"
+# print("Environment version label: " + os.environ.get("VERSION_LABEL"))
+# print(os.environ)
+# with open(path, "w") as file:
+#     file.write(str(version))
 
-with open(path, "r") as file:
-    text = file.read()
-
-version = int(text)
-version += 1
-os.environ["VERSION_LABEL"] = f"{version}"
-print("Environment version label: " + os.environ.get("VERSION_LABEL"))
-print(os.environ)
-with open(path, "w") as file:
-    file.write(str(version))
 
